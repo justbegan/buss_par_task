7c8ff532f42fbbbfadf9142c201ddca9cdd1af87
justbegan <72058184+justbegan@users.noreply.github.com>
2021-12-02 14:51:47 +0900

    big fix
    fix bugs

--- net_library/urls.py
+++ net_library/urls.py
@@ -4,7 +4,7 @@ from net_library import views
 open_book = views.Open_book()
 urlpatterns = [
     path('', views.main_page),
-    path('add_book', views.add_book, name='add_book'),
+    path('add_book', open_book.add_book, name='add_book'),
     path('change_book', open_book.change_book, name='change_book'),
     path('delete_book', open_book.delete_book, name='delete_book'),
     path('view_book/<int:pk>', open_book.view_book, name='view_book'),
--- net_library/views.py
+++ net_library/views.py
@@ -18,22 +18,9 @@ def main_page(request):
 
 
 
-
-@csrf_exempt
-def add_book(request):
-    post_date = request.POST.get
-    if len(post_date('title')) == 0 or len(post_date('content')) == 0:
-        return JsonResponse({'stat':0,},status=200)
-    b = Books(title=post_date('title'), author=post_date('author'),content=post_date('content') )
-    b.save()
-    book_id = Books.objects.values_list('pk').last()[0]
-    return JsonResponse({'stat':1, "value_list": [book_id, b.title, b.author, b.content]})
-
-
 class Open_book:
     last_select_book = 0
     def view_book(self, request, pk):
-        print(self.last_select_book)
         view_book = Books.objects.values().get(pk=pk)
         view_book['content_len'] = len(view_book['content'])
         self.last_select_book = pk
@@ -42,13 +29,31 @@ class Open_book:
     @csrf_exempt
     def change_book(self, request):
         post_date = request.POST.get
+        if not self.input_validation(post_date):
+            return JsonResponse({'stat': False,},status=200)
         obj = Books.objects.get(pk=self.last_select_book)
         obj.title, obj.author, obj.content = post_date('title'), post_date('author'), post_date('content')
         obj.save()
         return JsonResponse({"status":True})
 
+    @csrf_exempt
+    def add_book(self, request):
+        post_date = request.POST.get
+        if not self.input_validation(post_date):
+            return JsonResponse({'stat': False,},status=200)
+        b = Books(title=post_date('title'), author=post_date('author'),content=post_date('content') )
+        b.save()
+        book_id = Books.objects.values_list('pk').last()[0]
+        return JsonResponse({'stat':True, "value_list": [book_id, b.title, b.author, b.content]})
+
+    def input_validation(self, val_request):
+        if len(val_request('title')) == 0 or len(val_request('content')) == 0:
+            return False
+        else:
+            return True
 
     def delete_book(self, request):
-        print(self.last_select_book)
         Books.objects.get(pk=self.last_select_book).delete()
         return JsonResponse({"id":self.last_select_book})
+
+        
